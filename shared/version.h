ead61ea9fcef8971e534cba897f4fd25b3cdc1b6
JKA1988@gmail.com <JKA1988@gmail.com@a6a21e37-8a44-e1ba-01e6-af2beb0a85e6>
2013-10-23 13:19:03 +0000

    *Versioning ** Added minor version ability for release build. ** Updated version rules, please read comments in version.h.
    *Titlebar
    ** Updated the titlebar to reflect the change in versioning.
    ** Changed name on the titlebar from Hourglass to Hourglass-Resurrection.

--- shared/version.h
+++ shared/version.h
@@ -1,5 +1,14 @@
 #pragma once
 
-// Bump this every time before committing a change that is not purely cosmetic or in other ways not related to the
-// interaction between the game, the dll or Hourglass!
-#define VERSION 0x0001
+// VERSION and MINORVERSION usage:
+// VERSION is the most important define, it should be bumped everytime a change is made that can affect movie sync in any way. This is the part of
+// the version number that we check against in the movies.
+// MINORVERSION is for other fixes, like a crashing dialog fix etc, things that need to be fixed quickly, but doesn't affect movie sync in any way.
+// This number is purely cosmetic and is only used to tell fix-builds from eachother.
+// Every time VERSION is bumped, MINORVERSION should be reset to 0.
+
+// This versioning is for release-builds only, it is not encouraged to use interim builds for TASing as they may contain unfinished features
+// and other works in progress that can severly hamper the functionality of the program.
+
+#define VERSION 1
+#define MINORVERSION 0
