74385f275f8848c578d307e499cf924bdf2b4ce0
Ivan Molodetskikh <yalterz@gmail.com>
2016-01-17 16:19:16 +0300

    DLL: Finalized and fixed the XInput hooks.

--- wintasee/tramps/Xinputtramps.h
+++ wintasee/tramps/Xinputtramps.h
@@ -10,7 +10,7 @@
 #define XInputGetState TrampXInputGetState
 TRAMPFUNC DWORD WINAPI XInputGetState(
     DWORD         dwUserIndex,
-	XINPUT_STATE* pState
+    XINPUT_STATE* pState
 ) TRAMPOLINE_DEF
 
 #define XInputSetState TrampXInputSetState
@@ -27,10 +27,29 @@ TRAMPFUNC DWORD WINAPI XInputGetCapabilities(
 ) TRAMPOLINE_DEF
 
 #define XInputGetDSoundAudioDeviceGuids TrampXInputGetDSoundAudioDeviceGuids
-DWORD WINAPI XInputGetDSoundAudioDeviceGuids(
+TRAMPFUNC DWORD WINAPI XInputGetDSoundAudioDeviceGuids(
     DWORD dwUserIndex,          // [in] Index of the gamer associated with the device
     GUID* pDSoundRenderGuid,    // [out] DSound device ID for render
     GUID* pDSoundCaptureGuid    // [out] DSound device ID for capture
+) TRAMPOLINE_DEF
+
+#define XInputGetKeystroke TrampXInputGetKeystroke
+TRAMPFUNC DWORD WINAPI XInputGetKeystroke(
+    DWORD dwUserIndex,  // [in] Index of the gamer associated with the device
+    DWORD dwReserved,   // [in] Reserved
+    XINPUT_KEYSTROKE* pKeystroke    // [out] Receives the keystroke
+) TRAMPOLINE_DEF
+
+#define XInputEnable TrampXInputEnable
+TRAMPFUNC void WINAPI XInputEnable(
+    BOOL enable // [in] Indicates whether xinput is enabled or disabled.
+) TRAMPOLINE_DEF_VOID
+
+#define XInputGetBatteryInformation TrampXInputGetBatteryInformation
+TRAMPFUNC DWORD WINAPI XInputGetBatteryInformation(
+    DWORD dwUserIndex,  // [in] Index of the gamer associated with the device
+    BYTE devType,       // [in] Which device on this user index
+    XINPUT_BATTERY_INFORMATION* pBatteryInformation  // [out] Contains the level and types of batteries
 ) TRAMPOLINE_DEF
 
 #endif
