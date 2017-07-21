c8d509b546390d1b8dcc72423b49a5e5f8c0a805
Ivan Molodetskikh <yalterz@gmail.com>
2017-07-21 22:58:59 +0300

    Update code style

--- source/application/DbgHelp/DiaEnumIterator.h
+++ source/application/DbgHelp/DiaEnumIterator.h
@@ -11,7 +11,9 @@
 #pragma once
 #include <type_traits>
 
-namespace impl
+#include <DIA SDK/include/dia2.h>
+
+namespace DiaEnumIteratorImpl
 {
     /*
      * This little bit of template magic figures out the item type for a given DIA enumerator.
@@ -72,88 +74,100 @@ namespace impl
     {
         return CallItem(enumerator, &Enumerator::Item, index, item);
     }
+
+    /*
+     * An enum for representing the iterator's starting state.
+     */
+    enum class StartingState
+    {
+        Begin,
+        End
+    };
+
+    /*
+     * The iterator itself, implementing prefix ++, * and != as needed for the range-based for.
+     */
+    template<class Enumerator>
+    class DiaEnumIterator
+    {
+    public:
+        using item_type = item_type<Enumerator>;
+
+        DiaEnumIterator(CComPtr<Enumerator> enumerator, StartingState starting_state)
+            : m_enumerator(enumerator)
+            , m_count(GetCount<Enumerator>(enumerator))
+            , m_next_index(starting_state == StartingState::Begin ? 0 : m_count)
+            , m_last_item(nullptr)
+        {
+            GetNextItem();
+        }
+
+        inline const CComPtr<item_type>& operator++()
+        {
+            GetNextItem();
+            return m_last_item;
+        }
+
+        inline const CComPtr<item_type>& operator*()
+        {
+            return m_last_item;
+        }
+
+        inline bool operator!=(const DiaEnumIterator<Enumerator>& rhs)
+        {
+            return m_last_item != rhs.m_last_item;
+        }
+
+    private:
+        const CComPtr<Enumerator> m_enumerator;
+        const ULONG m_count;
+        DWORD m_next_index;
+        CComPtr<item_type> m_last_item;
+
+        void GetNextItem()
+        {
+            m_last_item = nullptr;
+
+            if (m_next_index == m_count)
+            {
+                return;
+            }
+
+            item_type* item;
+            if (ItemByIndex<Enumerator, item_type>(m_enumerator, m_next_index++, &item) == S_OK)
+            {
+                m_last_item = item;
+                item->Release();
+            }
+        }
+    };
 }
-
-/*
- * The iterator itself, implementing prefix ++, * and != as needed for the range-based for.
- */
-template<class Enumerator>
-class DiaEnumIterator
-{
-    using item_type = impl::item_type<Enumerator>;
-
-    const CComPtr<Enumerator> m_enumerator;
-    const ULONG m_count;
-    DWORD m_next_index;
-    CComPtr<item_type> m_last_item;
-
-    void GetNextItem()
-    {
-        m_last_item = nullptr;
-
-        if (m_next_index == m_count)
-        {
-            return;
-        }
-
-        item_type* item;
-        if (impl::ItemByIndex<Enumerator, item_type>(m_enumerator, m_next_index++, &item) == S_OK)
-        {
-            m_last_item = item;
-            item->Release();
-        }
-    }
-
-public:
-    DiaEnumIterator(CComPtr<Enumerator> enumerator, DWORD index)
-        : m_enumerator(enumerator)
-        , m_count(impl::GetCount<Enumerator>(enumerator))
-        , m_next_index(index)
-        , m_last_item(nullptr)
-    {
-        GetNextItem();
-    }
-
-    inline const CComPtr<item_type>& operator++()
-    {
-        GetNextItem();
-        return m_last_item;
-    }
-
-    inline const CComPtr<item_type>& operator*()
-    {
-        return m_last_item;
-    }
-
-    inline bool operator!=(const DiaEnumIterator<Enumerator>& rhs)
-    {
-        return m_last_item != rhs.m_last_item;
-    }
-};
 
 /*
  * The begin() and end() functions that the range-based for loop uses.
  */
 template<class Enumerator>
-DiaEnumIterator<Enumerator> begin(Enumerator* enumerator)
+DiaEnumIteratorImpl::DiaEnumIterator<Enumerator> begin(Enumerator* enumerator)
 {
-    return DiaEnumIterator<Enumerator>(enumerator, 0);
+    return DiaEnumIteratorImpl::DiaEnumIterator<Enumerator>(enumerator, DiaEnumIteratorImpl::StartingState::Begin);
 }
 
 template<class Enumerator>
-DiaEnumIterator<Enumerator> end(Enumerator* enumerator)
+DiaEnumIteratorImpl::DiaEnumIterator<Enumerator> end(Enumerator* enumerator)
 {
-    return DiaEnumIterator<Enumerator>(enumerator, impl::GetCount(enumerator));
+    return DiaEnumIteratorImpl::DiaEnumIterator<Enumerator>(enumerator, DiaEnumIteratorImpl::StartingState::End);
 }
 
 template<class Enumerator>
-DiaEnumIterator<Enumerator> begin(CComPtr<Enumerator> enumerator)
+DiaEnumIteratorImpl::DiaEnumIterator<Enumerator> begin(CComPtr<Enumerator> enumerator)
 {
-    return DiaEnumIterator<Enumerator>(std::move(enumerator), 0);
+    return DiaEnumIteratorImpl::DiaEnumIterator<Enumerator>(std::move(enumerator),
+                                                            DiaEnumIteratorImpl::StartingState::Begin);
 }
 
 template<class Enumerator>
-DiaEnumIterator<Enumerator> end(CComPtr<Enumerator> enumerator)
+DiaEnumIteratorImpl::DiaEnumIterator<Enumerator> end(CComPtr<Enumerator> enumerator)
 {
-    return DiaEnumIterator<Enumerator>(std::move(enumerator), impl::GetCount<Enumerator>(enumerator));
+    return DiaEnumIteratorImpl::DiaEnumIterator<Enumerator>(std::move(enumerator),
+                                                            DiaEnumIteratorImpl::StartingState::End);
 }
