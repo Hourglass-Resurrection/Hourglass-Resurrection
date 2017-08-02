2eb7219d9a5b52eddf9eaeed46220292c2b23de1
Warepire <Warepire@users.noreply.github.com>
2017-08-03 00:06:29 +0200

    Utilize C++14 constexpr in LogCategoryToString

--- source/hooks/print.h
+++ source/hooks/print.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2011 nitsuja and contributors
+﻿/*  Copyright (C) 2011 nitsuja and contributors
     Hourglass is licensed under GPL v2. Full notice is in COPYING.txt. */
 
 #pragma once
@@ -64,7 +64,60 @@ namespace Hooks
     int getCurrentTimestamp();
 }
 
-/*constexpr*/ const char* LogCategoryToString(LogCategory category);
+constexpr const char* LogCategoryToString(LogCategory category)
+{
+    switch (category)
+    {
+    case LogCategory::ANY:
+        return "";
+    case LogCategory::HOOK:
+        return "[Hook]";
+    case LogCategory::TIME:
+        return "[Time]";
+    case LogCategory::DETTIMER:
+        return "[Det. Timer]";
+    case LogCategory::SYNC:
+        return "[Synchronization]";
+    case LogCategory::DDRAW:
+        return "[DDraw]";
+    case LogCategory::D3D:
+        return "[D3D]";
+    case LogCategory::OGL:
+        return "[OpenGL:TODO:Stop wrapping D3D!]";
+    case LogCategory::GDI:
+        return "[GDI]";
+    case LogCategory::SDL:
+        return "[SDL:TODO:Remove!]";
+    case LogCategory::DINPUT:
+        return "[DInput]";
+    case LogCategory::WINPUT:
+        return "[WinInput]";
+    case LogCategory::XINPUT:
+        return "[XInput]";
+    case LogCategory::DSOUND:
+        return "[DSound";
+    case LogCategory::WSOUND:
+        return "[WSound]";
+    case LogCategory::PROCESS:
+        return "[Process]";
+    case LogCategory::MODULE:
+        return "[Module]";
+    case LogCategory::MESSAGES:
+        return "[Messages]";
+    case LogCategory::WINDOW:
+        return "[Window]";
+    case LogCategory::FILEIO:
+        return "[File I/O]";
+    case LogCategory::REGISTRY:
+        return "[Registry]";
+    case LogCategory::THREAD:
+        return "[Thread]";
+    case LogCategory::TIMERS:
+        return "[Timers]";
+    default:
+        return "[Unknown]";
+    }
+}
 
 template<LogCategory category = LogCategory::ANY>
 class DebugLog
