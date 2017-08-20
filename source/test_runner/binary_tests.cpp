e73246f4c85e3ee8dfc854f990befb68ebf324e7
Ivan Molodetskikh <yalterz@gmail.com>
2017-08-20 10:19:28 +0300

    Add log comparison and output to the test runner

--- source/test_runner/binary_tests.cpp
+++ source/test_runner/binary_tests.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <vector>
@@ -16,6 +17,8 @@ namespace BinaryTests
     namespace
     {
         constexpr char TEMP_DIR_NAME[] = "temp";
+        constexpr char OUTPUT_LOG_FILENAME[] = "output.log";
+        constexpr char FAILED_DIR_NAME[] = "failed";
 
         struct InputPair
         {
@@ -45,6 +48,53 @@ namespace BinaryTests
             filesystem::copy(folder, gs_base_path / TEMP_DIR_NAME, filesystem::copy_options::recursive);
         }
 
+        std::vector<std::wstring> ReadLog(const filesystem::path& path)
+        {
+            std::vector<std::wstring> rv;
+
+            std::wifstream file(path);
+            std::wstring line;
+            while (std::getline(file, line))
+                rv.push_back(std::move(line));
+
+            return rv;
+        }
+
+        bool CompareLogs(const filesystem::path& expected, const filesystem::path& output)
+        {
+            auto expected_lines = ReadLog(expected);
+            auto output_lines = ReadLog(output);
+
+            bool logs_match = true;
+
+            if (expected_lines.size() == output_lines.size())
+            {
+                for (size_t i = 0; i < expected_lines.size(); ++i)
+                {
+                    auto expected_line = expected_lines[i];
+                    auto output_line = output_lines[i];
+
+                    if (expected_line != output_line)
+                    {
+                        std::wcout << L"Log mismatch detected on line " << i
+                                   << L":\nExpected: " << expected_line
+                                   << L"\nOutput:   " << output_line << L'\n';
+
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                std::wcout << L"The log has a wrong line count. Expected: " << expected_lines.size()
+                           << L"; got: " << output_lines.size() << L'\n';
+
+                return false;
+            }
+
+            return true;
+        }
+
         TEST_CASE( "binary tests" )
         {
             for (const auto& test : gs_tests)
@@ -58,10 +108,46 @@ namespace BinaryTests
                     {
                         SECTION( input_pair.input.filename().string() )
                         {
+                            /*
+                             * Create a temp folder and copy the test there.
+                             */
                             TempDir temp_dir;
                             CopyContentsToTempDir(test.first);
 
+                            /*
+                             * Run Hourglass on the test .exe with the current input file.
+                             */
                             REQUIRE_NOTHROW( Hourglass::Run(exe_path, input_pair.input) );
+
+                            /*
+                             * Make sure the test output its log file.
+                             */
+                            const filesystem::path output_log = gs_base_path / TEMP_DIR_NAME / OUTPUT_LOG_FILENAME;
+                            REQUIRE( filesystem::exists(output_log) );
+
+                            /*
+                             * Compare the test output with the expected output.
+                             */
+                            const bool logs_match = CompareLogs(input_pair.log, output_log);
+
+                            /*
+                             * If the logs do not match, save the output log for further inspection.
+                             */
+                            if (!logs_match)
+                            {
+                                const filesystem::path failed = gs_base_path / FAILED_DIR_NAME / test.first.filename();
+                                filesystem::create_directories(failed);
+
+                                filesystem::path expected = failed / input_pair.log.filename();
+                                expected.replace_filename(expected.stem().wstring() + L"_expected.log");
+
+                                filesystem::path output = failed / (input_pair.log.stem().wstring() + L"_output.log");
+
+                                filesystem::copy_file(input_pair.log, expected);
+                                filesystem::copy_file(output_log, output);
+                            }
+
+                            REQUIRE( logs_match );
                         }
                     }
                 }
