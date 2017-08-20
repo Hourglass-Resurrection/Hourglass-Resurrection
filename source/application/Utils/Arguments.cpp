404e6e9e6d1953db305e0141eee03ac172baefa3
Ivan Molodetskikh <yalterz@gmail.com>
2017-08-20 12:02:14 +0300

    Add missing files

---
+++ source/application/Utils/Arguments.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017- Hourglass Resurrection Team
+ * Hourglass Resurrection is licensed under GPL v2.
+ * Refer to the file COPYING.txt in the project root.
+ */
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <shellapi.h>
+
+#include "Arguments.h"
+
+namespace Arguments
+{
+    namespace
+    {
+        void ParseInternal(int argc, LPWSTR argv[])
+        {
+            for (int i = 0; i < argc; ++i)
+            {
+                std::wstring arg = argv[i];
+
+                if (arg == L"--play")
+                {
+                    g_args.m_play = true;
+                }
+                else if (arg == L"--quit-on-movie-end")
+                {
+                    g_args.m_quit_on_movie_end = true;
+                }
+                else if (arg == L"--game" && i + 1 < argc)
+                {
+                    g_args.m_game_filename = argv[++i];
+                }
+                else if (arg == L"--movie" && i + 1 < argc)
+                {
+                    g_args.m_movie_filename = argv[++i];
+                }
+            }
+        }
+    }
+
+    Arguments g_args{};
+
+    void Parse(LPCWSTR lpCmdLine)
+    {
+        int argc;
+        LPWSTR* argv = CommandLineToArgvW(lpCmdLine, &argc);
+
+        if (argv)
+        {
+            ParseInternal(argc, argv);
+            LocalFree(argv);
+        }
+    }
+}
---
+++ source/application/Utils/Arguments.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017- Hourglass Resurrection Team
+ * Hourglass Resurrection is licensed under GPL v2.
+ * Refer to the file COPYING.txt in the project root.
+ */
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <optional>
+#include <string>
+
+namespace Arguments
+{
+    struct Arguments
+    {
+        std::optional<std::wstring> m_game_filename;
+        std::optional<std::wstring> m_movie_filename;
+        bool m_play;
+        bool m_quit_on_movie_end;
+    };
+
+    extern Arguments g_args;
+
+    void Parse(LPCWSTR lpCmdLine);
+}
