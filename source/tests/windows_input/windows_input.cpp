dd92639bc0053d3866f0652456752554c339b678
Ivan Molodetskikh <yalterz@gmail.com>
2017-08-26 14:15:44 +0300

    Add a windows_input test (no test input yet)

---
+++ source/tests/windows_input/windows_input.cpp
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2017- Hourglass Resurrection Team
+ * Hourglass Resurrection is licensed under GPL v2.
+ * Refer to the file COPYING.txt in the project root.
+ */
+
+#include "stdafx.h"
+#include "input.h"
+
+constexpr WCHAR CLASS_NAME[] = L"windows_input";
+constexpr WCHAR WINDOW_NAME[] = L"windows_input";
+
+static uint32_t gs_pixel = 0x000000CC;
+static BITMAPINFO gs_bitmap_info;
+
+ATOM             MyRegisterClass(HINSTANCE);
+HWND             InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    HWND hWnd = InitInstance(hInstance, nCmdShow);
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    {
+        BITMAPINFOHEADER& header = gs_bitmap_info.bmiHeader;
+        header.biSize = sizeof(header);
+        header.biWidth = 1;
+        header.biHeight = 1;
+        header.biPlanes = 1;
+        header.biBitCount = 32;
+        header.biCompression = BI_RGB;
+    }
+
+    MSG msg;
+
+    // Main message loop:
+    bool running = true;
+    while (running)
+    {
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                running = false;
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        Input::PreRender();
+
+        /*
+         * Force a Hourglass frame boundary.
+         */
+        HDC hdc = GetDC(hWnd);
+        SwapBuffers(hdc);
+        ReleaseDC(hWnd, hdc);
+
+        Input::PostRender();
+    }
+
+    return (int) msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex{};
+
+    wcex.cbSize        = sizeof(wcex);
+    wcex.style         = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc   = WndProc;
+    wcex.hInstance     = hInstance;
+    wcex.hCursor       = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1);
+    wcex.lpszClassName = CLASS_NAME;
+
+    return RegisterClassExW(&wcex);
+}
+
+HWND InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd = CreateWindowW(CLASS_NAME,
+                             WINDOW_NAME,
+                             WS_OVERLAPPEDWINDOW,
+                             CW_USEDEFAULT,
+                             0,
+                             CW_USEDEFAULT,
+                             0,
+                             nullptr,
+                             nullptr,
+                             hInstance,
+                             nullptr);
+
+   if (!hWnd)
+   {
+      return nullptr;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return hWnd;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
